58acffd256a22b0eb94a5dc0f448e9beff1f1568
The-Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-02-29 13:23:33 +0900

    make cam app

---
+++ cam/sw.js
@@ -0,0 +1,29 @@
+// Cache name
+const CACHE_NAME = 'future-shell';
+// Cache targets
+const urlsToCache = [
+  './',
+  './index.html',
+  './icon.png',
+  './forPWA.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        return response ? response : fetch(event.request);
+      })
+  );
+});

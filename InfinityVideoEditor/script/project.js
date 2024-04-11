e16e8a7ff2b23ea0f1b003e7baa46bec0abc42ae
The-Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-04-11 10:54:55 +0900

    start test project.elem.add

--- InfinityVideoEditor/script/project.js
+++ InfinityVideoEditor/script/project.js
@@ -1,45 +1,54 @@
-const elem = {
-  type: null, //video image text audioのどれかが入る
-  start: 0,
-  length: 0,
-  animation: {
-    0: {
-      data: {
-        pos: {
-          x: 0,
-          y: 0,
-        },
-        direction: 0,
-        size: 100,
-        effects: {},
-      },
-      easing: {
-        type: "linear",
-        args: {},
-      },
-    },
-    1: {
-      data: {
-        pos: {
-          x: 0,
-          y: 0,
-        },
-        direction: 0,
-        size: 100,
-        effects: {},
-      },
-      easing: {
-        type: "linear",
-        args: {},
-      },
-    },
-  },
-};
-
 class element {
   constructor(type) {
-    this.info = elem;
-    this.buttom = document.createElement("buttom");
+    this.info = {
+      type: type, //video image text audioのどれかが入る
+      start: project.currentTime(),
+      length: 1,
+      layer:1,
+      animation: {
+        0: {
+          data: {
+            pos: {
+              x: 0,
+              y: 0,
+            },
+            direction: 0,
+            size: 100,
+            effects: {},
+          },
+          easing: {
+            type: "linear",
+            args: {},
+          },
+        },
+        1: {
+          data: {
+            pos: {
+              x: 0,
+              y: 0,
+            },
+            direction: 0,
+            size: 100,
+            effects: {},
+          },
+          easing: {
+            type: "linear",
+            args: {},
+          },
+        },
+      },
+    };
+    let elem=document.createElement("buttom");
+    elem.className="elem_buttom";
+    this.elem=elem;
+    this.renewElem();
+    document.querySelector("#selector-main").appendChild(this.elem);
+  }
+  renewElem(){
+    this.elem.style.setProperty("--elem-type","var(--elem-type-"+this.info.type+")");
+    this.elem.style.setProperty("--length",this.info.length);
+    this.elem.style.setProperty("--start",this.info.start);
+    this.elem.style.setProperty("--layer",this.info.layer);
   }
 }
 

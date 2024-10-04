7519b2d965fc40099e93efb42ea321b781ca28da
The Infinity's <137460810+The-Infinitys@users.noreply.github.com>
2024-10-04 11:05:47 +0900

    動いてる...?

--- synth-generator/script.js
+++ synth-generator/script.js
@@ -1,6 +1,7 @@
 document
   .querySelector("div.main>div.ui>div.overtone>div.control>div.manage>button")
   .addEventListener("click", () => {
+    console.log(123);
     const overtone_input = document.createElement("input");
     overtone_input.type = "range";
     document

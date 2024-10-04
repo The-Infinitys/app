afabaf1bebaf5c1615f210ec09986a6c01661752
The Infinity's <137460810+The-Infinitys@users.noreply.github.com>
2024-10-04 11:13:50 +0900

    一旦保存

--- synth-generator/script.js
+++ synth-generator/script.js
@@ -1,32 +1,33 @@
-document
-  .querySelector(
-    "div.main>div.ui>div.overtone>div.control>div.manage>button.add"
-  )
-  .addEventListener("click", () => {
-    console.log("add new parameter");
-    const overtone_input = document.createElement("input");
-    overtone_input.type = "range";
-    document
-      .querySelector("div.main>div.ui>div.overtone>div.control")
-      .insertBefore(
-        overtone_input,
-        document.querySelector(
-          "div.main>div.ui>div.overtone>div.control>div.manage"
-        )
+const overtone = () => {
+  document
+    .querySelector(
+      "div.main>div.ui>div.overtone>div.control>div.manage>button.add"
+    )
+    .addEventListener("click", () => {
+      console.log("add new parameter");
+      const overtone_input = document.createElement("input");
+      overtone_input.type = "range";
+      document
+        .querySelector("div.main>div.ui>div.overtone>div.control")
+        .insertBefore(
+          overtone_input,
+          document.querySelector(
+            "div.main>div.ui>div.overtone>div.control>div.manage"
+          )
+        );
+    });
+  document
+    .querySelector(
+      "div.main>div.ui>div.overtone>div.control>div.manage>button.remove"
+    )
+    .addEventListener("click", () => {
+      console.log("remove parameter");
+      const control = document.querySelector(
+        "div.main>div.ui>div.overtone>div.control"
       );
-  });
-document
-  .querySelector(
-    "div.main>div.ui>div.overtone>div.control>div.manage>button.remove"
-  )
-  .addEventListener("click", () => {
-    console.log("remove parameter");
-    const control = document.querySelector(
-      "div.main>div.ui>div.overtone>div.control"
-    );
-    if (control.children.length > 2) {
-      control.children[
-        control.children.lengthcontrol.children.length - 2
-      ].remove();
-    }
-  });
+      if (control.children.length > 2) {
+        control.children[control.children.length - 2].remove();
+      }
+    });
+};
+overtone();

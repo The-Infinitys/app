8e6a2bee46d2ec34595e2cd4792a53f3e3d37f42
The Infinity's <137460810+The-Infinitys@users.noreply.github.com>
2024-10-04 11:09:07 +0900

    なぜか動かない

--- synth-generator/script.js
+++ synth-generator/script.js
@@ -1,15 +1,32 @@
 document
-  .querySelector("div.main>div.ui>div.overtone>div.control>div.manage>button")
+  .querySelector(
+    "div.main>div.ui>div.overtone>div.control>div.manage>button.add"
+  )
   .addEventListener("click", () => {
-    console.log(123);
+    console.log("add new parameter");
     const overtone_input = document.createElement("input");
     overtone_input.type = "range";
     document
       .querySelector("div.main>div.ui>div.overtone>div.control")
-      .append(
+      .insertBefore(
         overtone_input,
         document.querySelector(
           "div.main>div.ui>div.overtone>div.control>div.manage"
         )
       );
+  });
+document
+  .querySelector(
+    "div.main>div.ui>div.overtone>div.control>div.manage>button.remove"
+  )
+  .addEventListener("click", () => {
+    console.log("remove parameter");
+    const control = document.querySelector(
+      "div.main>div.ui>div.overtone>div.control"
+    );
+    if (control.children.length > 2) {
+      control.children[
+        control.children.lengthcontrol.children.length - 2
+      ].remove();
+    }
   });
